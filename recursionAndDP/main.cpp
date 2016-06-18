f1d79e068e6a98c889371244abae368aec9f77df
shazzing <kewlfundas@gmail.com>
2016-06-18 17:53:08 -0400

    initial commit2.

--- recursionAndDP/main.cpp
+++ recursionAndDP/main.cpp
@@ -7,9 +7,20 @@
 //
 
 #include <iostream>
+#include "recursionDP1.h"
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int m_steps = steps(7);
+    cout << " num of ways to cover 7 steps are : " << m_steps << endl;
+    vector<vector<int>> grid(4, vector<int>(4, 0));
+    grid[1][0] = 1, grid[0][2] = 1, grid[1][2] = 1, grid[2][3] = 1, grid[3][1] = 1;
+    vector<pair<int, int>> rob_path = robotPath(grid);
+    for(pair<int, int> pr:rob_path){
+        cout << "{" << pr.first << "," << pr.second << "}, " << endl;
+    }
+    cout << endl;
     return 0;
 }
